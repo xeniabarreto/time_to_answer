30e389efb3aaa0f031e6e4c44aa06c607be53e01
xeniabarreto <xeniabarreto22@gmail.com>
2023-06-29 13:25:45 +0200

    fix: delete profile endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
 
   get 'inicio', to: 'site/welcome#index'
